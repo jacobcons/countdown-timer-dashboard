e4066018cb007a05492590b5afa9d813ad937e55
Jacob <jacobcons@gmail.com>
2024-08-11 18:11:39 +0000

    time components

--- src/components/TimeComponentInput.vue
+++ src/components/TimeComponentInput.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >Hours:</label
+    <label
+      for="number-input"
+      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >{{ label }}</label
     >
     <input
       type="number"
       id="number-input"
       aria-describedby="helper-text-explanation"
       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="0"
       min="0"
       required
+      v-model="componentValue"
     />
   </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps(['label'])
+const componentValue = defineModel()
+</script>
--- src/components/TimerForm.vue
+++ src/components/TimerForm.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h2 class="text-lg mb-4">Form</h2>
-    <form class="grid grid-cols-3">
-      <TimeComponentInput />
-      <TimeComponentInput />
-      <TimeComponentInput />
+    <form class="grid grid-cols-3 gap-4">
+      <TimeComponentInput label="Hours:" v-model="hours" />
+      <TimeComponentInput label="Minutes:" v-model="minutes" />
+      <TimeComponentInput label="Seconds:" v-model="seconds" />
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
 import TimeComponentInput from '@/components/TimeComponentInput.vue'
+import { ref } from 'vue'
+
+const hours = ref(0)
+const minutes = ref(0)
+const seconds = ref(0)
 </script>
