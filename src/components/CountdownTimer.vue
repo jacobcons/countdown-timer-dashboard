c083514e4bdf3f92f97f85a0276a2ddee81a3460
Jacob <jacobcons@gmail.com>
2024-08-13 17:56:57 +0000

    delete timer

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -1,15 +1,33 @@
 <template>
   <div
     href="#"
-    class="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800"
+    class="relative w-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800"
   >
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       {{ timer.label }}
     </h5>
     <p class="mb-2 font-normal text-gray-700 dark:text-gray-400">{{ formattedTimeLeft }}</p>
-    <TimerControl :func="startTimer" :timerId="timer.id" text="Start" />
-    <TimerControl :func="stopTimer" :timerId="timer.id" text="Stop" />
-    <TimerControl :func="resetTimer" :timerId="timer.id" text="Reset" />
+    <TimerControl :func="startTimer" text="Start" />
+    <TimerControl :func="stopTimer" text="Stop" />
+    <TimerControl :func="resetTimer" text="Reset" />
+    <svg
+      class="absolute right-4 top-4 h-6 w-6 cursor-pointer text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+      @click="deleteTimer"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+      />
+    </svg>
   </div>
 </template>
 
@@ -20,25 +38,20 @@ import { storeToRefs } from 'pinia'
 import TimerControl from '@/components/TimerControl.vue'
 import { computed } from 'vue'
 
-const { timer } = defineProps<{ timer: Timer }>()
-const { timers } = storeToRefs(useTimersStore())
+const props = defineProps<{ timer: Timer }>()
+let { timers } = storeToRefs(useTimersStore())
+const timer = timers.value.find((timer) => timer.id === props.timer.id) as Timer
 const formattedTimeLeft = computed(() => {
   // split seconds left into as many hours, minutes and then seconds as possible
-  const { secondsLeft } = timer
+  const { secondsLeft } = props.timer
   const SECONDS_IN_HOUR = 3600
-  const hours = Math.floor(secondsLeft / SECONDS_IN_HOUR)
-  const minutes = Math.floor((secondsLeft % SECONDS_IN_HOUR) / 60)
-  const seconds = secondsLeft % 60
+  const hours = String(Math.floor(secondsLeft / SECONDS_IN_HOUR)).padStart(2, '0')
+  const minutes = String(Math.floor((secondsLeft % SECONDS_IN_HOUR) / 60)).padStart(2, '0')
+  const seconds = String(secondsLeft % 60).padStart(2, '0')
   return `${hours}:${minutes}:${seconds}`
 })
 
-function findTimer(id: number) {
-  return timers.value.find((timer) => timer.id === id) as Timer
-}
-
-function startTimer(id: number) {
-  const timer = findTimer(id)
-
+function startTimer() {
   if (timer.started) {
     return
   }
@@ -52,18 +65,18 @@ function startTimer(id: number) {
   }, 1000)
 }
 
-function stopTimer(id: number) {
-  const timer = findTimer(id)
-
+function stopTimer() {
   clearInterval(timer.intervalId as number)
   timer.started = false
 }
 
-function resetTimer(id: number) {
-  const timer = findTimer(id)
-
+function resetTimer() {
   clearInterval(timer.intervalId as number)
   timer.started = false
   timer.secondsLeft = timer.startingSeconds
+}
+
+function deleteTimer() {
+  timers.value = timers.value.filter((timer) => timer.id !== props.timer.id)
 }
 </script>
--- src/components/TimerControl.vue
+++ src/components/TimerControl.vue
@@ -2,7 +2,7 @@
   <button
     type="button"
     class="mb-2 me-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-    @click="func(timerId)"
+    @click="func()"
   >
     {{ text }}
   </button>
@@ -10,7 +10,6 @@
 <script setup lang="ts">
 defineProps<{
   func: (id: number) => void
-  timerId: number
   text: string
 }>()
 </script>
